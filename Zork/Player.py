018c652eea9adbb6b7517bf9cbfaff93b07ba3ff
Ronald Rounsifer <ronrounsifer@me.com>
2018-03-13 19:32:09 -0400

    Starting on main class

--- Zork/Player.py
+++ Zork/Player.py
@@ -6,8 +6,8 @@ class Player(object):
 		super().__init__()
 		self.HP = randint(100,125)
 		self.attack = randint(10, 20)
-		self.weapons = [ Weapon(randint(0,3)) for y in range(10)]
-		self.current_weapon = self.weapons[0]
+		self.inventory = [ Weapon(randint(0,3)) for y in range(10)]
+		self.current_weapon = self.inventory[0]
 
 	def getHP(self):
 		return self.HP
@@ -18,13 +18,10 @@ class Player(object):
 	def getCurrentWeapon(self):
 		return self.current_weapon
 
-	def listWeapons(self):
-		for w in self.weapons:
+	def getInventory(self):
+		return self.inventory
+
+	def listInventory(self):
+		for w in self.inventory:
 			print(w.get_name())
-
-		
-p = Player()
-
-for w in p.weapons:
-	print(w.get_name())
 	
--- Zork/Zork.py
+++ Zork/Zork.py
@@ -1,12 +1,51 @@
 from House import House
 from Monster import Monster
 from Weapon import Weapon
+from Player import Player
 from Neighborhood import Neighborhood
+
+def promptForPlayStyle():
+	try:
+		print("\nWhich type of experience would you prefer?")
+		print("1) Automated")
+		print("2) Manual")	
+		return play_style
+	except Exception as e:
+		getPlayStyle()
+
+def promptForRows():
+	try:
+		rows = int(input("How many rows of houses are in your neighborhood? "))
+		return rows
+	except Exception as e:
+		get_rows_from_user()
+	
+
+def promptForCols():
+	try:
+		cols = int(input("Now, how many columns of houses are there? "))
+		return cols
+	except Exception as e:
+		get_cols_from_user()
+
 
 if __name__ == "__main__":
 
-	print("ZORK")
+	print("\nzorKK")
 
-	# Create neighborhood
-	n = Neighborhood(5,5)
+	play_style = promptForPlayStyle()
+	print('\n')
+	rows = promptForRows()
+	cols = promptForCols()
+	print("\n")
+
+	# create neighborhood instance
+	n = Neighborhood(rows,cols)
 	n.showNeighborhood()
+
+	# create a player instance
+	p = Player()
+	inventory = p.getInventory()
+
+
+	# travel from house to house, fighting monsters
