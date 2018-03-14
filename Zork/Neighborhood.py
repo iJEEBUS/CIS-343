4930ec2b24f42310a5ce8d65522fbced6cf0db53
Ronald Rounsifer <ronrounsifer@me.com>
2018-03-13 20:33:50 -0400

    Random monster distribution working

--- Zork/Neighborhood.py
+++ Zork/Neighborhood.py
@@ -11,25 +11,19 @@ class Neighborhood(Observer):
 		
 		row = 0
 
+		# appends houses to the neighborhood matrix
+		# each house contains a random number of monsters
 		for row in range(0, self.rows):
 			self.neighborhood.append([])
 			for col in range(0, self.cols):
 				self.neighborhood[row].append(House(randint(0,10)))
-			
 				self.neighborhood[row][col].add_observer(self)
 
-
 	def showNeighborhood(self):
-		row_count, col_count = 0, 0
 
-		for row in self.neighborhood:
+		for row in range(0, self.rows):
+			for col in range(0,self.cols):
+				print(self.neighborhood[row][col].numMonsters(), end=" ")
 
-			for col in row:
-				col_count = 0
-				if col_count < self.cols:
-					print(self.neighborhood[row_count][col_count].numMonsters(), end=" ")
-					col_count += 1
 				#print("\x1b[37m\u2588", end=" ")
-			if row_count < self.rows:
-				row_count += 1
 			print('\n')
